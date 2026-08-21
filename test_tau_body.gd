b3347ac53f4fbf8a8d757f5ade4a901c508a1539
David <david@ideatomik.net>
2026-08-21 16:34:15 -0300

    tau mess

--- test_tau_body.gd
+++ test_tau_body.gd
@@ -1,21 +1,41 @@
 extends Node2D
 
-@export var helicase_dummy: Node2D
-@export var lagging_poly_dummy: Node2D
-@export var tau_body: Node2D
+@export_group("Tau Body Visuals")
+@export var body_color: Color = Color(0.8, 0.6, 0.2, 0.9)
+@export var connector_color: Color = Color(0.5, 0.5, 0.5, 0.7)
+@export var body_size: float = 24.0
+@export var connector_width: float = 4.0
+@export var connector_count: int = 3
+@export var pre_loop_min: float = 10.0
+@export var loop_tightness: float = 0.5
 
-func _ready() -> void:
+var helicase_pos: Vector2 = Vector2.ZERO
+var lagging_pos: Vector2 = Vector2.ZERO
+
+func _ready():
 	pass
 
-func _process(_delta: float) -> void:
-	if not helicase_dummy or not lagging_poly_dummy or not tau_body:
-		return
+func _process(_delta):
+	queue_redraw()
 
-	var h_pos = helicase_dummy.global_position
-	var l_pos = lagging_poly_dummy.global_position
-	var midpoint = (h_pos + l_pos) * 0.5
+func set_connections(h_pos: Vector2, l_pos: Vector2):
+	helicase_pos = h_pos
+	lagging_pos = l_pos
+	queue_redraw()
 
-	tau_body.global_position = midpoint
+func _draw():
+	var local_h = to_local(helicase_pos)
+	var local_l = to_local(lagging_pos)
 
-	if tau_body.has_method("set_connections"):
-		tau_body.set_connections(h_pos, l_pos)
+	for i in range(connector_count):
+		var offset = Vector2(randf() - 0.5, randf() - 0.5) * 10.0
+		var start_point = local_h + offset
+		var end_point = local_l + offset
+
+		draw_line(start_point, end_point, connector_color, connector_width, true)
+
+	var midpoint = (local_h + local_l) / 2.0
+	draw_circle(midpoint, body_size / 2.0, body_color)
+
+	draw_circle(local_h, 6.0, Color.GREEN, true)
+	draw_circle(local_l, 6.0, Color.RED, true)
